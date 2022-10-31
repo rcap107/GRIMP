60ffef93bef53eb1ef892a9eaadff50e57349878
Riccardo Cappuzzo <cappuzzo@eurecom.fr>
2022-10-31 17:06:20 +0100

    Extended script to fetch the model if not present.

--- prepare_pretrained_embeddings.py
+++ prepare_pretrained_embeddings.py
@@ -1,17 +1,43 @@
+"""This script prepares pre-trained embeddings for all nodes in the graph 
+(rows, values, column edges) starting from the fasttext pre-trained embeddings. 
+The embeddings are produced by using the fasttext get_sentence_vector 
+function. 
+
+Author: Riccardo Cappuzzo
+"""
 import pandas as pd
 import fasttext
 import numpy as np
 from tqdm import tqdm
 import os.path as osp
 import os
+from fasttext.util import download_model
+import shutil
+import sys
 
 
+def prepare_ft_model():
+    fname = download_model(lang_id='en', if_exists='ignore')
+    os.makedirs('data/', exist_ok=True)
+    new_fname = osp.join('data', fname)
+    shutil.move(fname, new_fname)
+    return new_fname
+    
 def generate(df, model, n_dim=300):
+    """Given the target dataframe and a pre-trained model, produce embeddings for
+    all the entities in the dataframe. 
+
+    Args:
+        df (pd.DataFrame): Target dataframe to convert. 
+        model (_type_): Fasttext pre-trained model.
+        n_dim (int, optional): Number of dimensions to use for the embeddings. Defaults to 300.
+    """
+    
     # Replace '-' with spaces for sentence emb generation
     for col in df.columns:
         df[col] = df[col].str.replace('-', ' ')
 
-    # Add to each value in the dataset the column they belong to.
+    # Disambiguate values in the dataframe by prefixing them with the column name. 
     for idx, col in enumerate(df.columns):
         try:
             df[col] = df[col].astype(float).round(8)
@@ -49,10 +75,10 @@ def generate(df, model, n_dim=300):
 
     for idx, row in df.iterrows():
         tmp_vec = np.zeros(shape=(df.shape[1], n_dim))
-        for ri, w in enumerate(row):
-            w = str(w)
-            vector = vector_dict[w]
-            tmp_vec[ri] = vector
+        for row_id, word in enumerate(row):
+            word = str(word)
+            vector = vector_dict[word]
+            tmp_vec[row_id] = vector
         row_vectors[idx] = np.mean(tmp_vec, 0)
 
     print('Generating column embeddings.')
@@ -60,13 +86,13 @@ def generate(df, model, n_dim=300):
 
     for idx, col in enumerate(df.columns):
         tmp_vec = np.zeros(shape=(df.shape[0], n_dim))
-        for ri, w in enumerate(df[col]):
-            vector = vector_dict[str(w)]
-            tmp_vec[ri] = vector
+        for row_id, word in enumerate(df[col]):
+            vector = vector_dict[str(word)]
+            tmp_vec[row_id] = vector
         col_vectors[col] = np.mean(tmp_vec, 0)
 
     print('Writing embeddings on file. ')
-    with open(generated_emb_file, 'w') as fp:
+    with open(generated_emb_file, 'w', encoding='str') as fp:
         tot_rows = len(row_vectors) + len(col_vectors) + len(vector_dict)
         fp.write(f'{tot_rows} {n_dim}\n')
         for k, vec in tqdm(row_vectors.items(), total=len(row_vectors)):
@@ -83,8 +109,21 @@ def generate(df, model, n_dim=300):
 
 
 if __name__ == '__main__':
-    # Load fasttext model once for all datasets.
     fname = 'data/cc.en.300.bin'
+    
+    # Fetching the model if it is not currently available. 
+    if not osp.exists(fname):
+        print(f'Pre-trained model not found on path {fname}.')
+        reply = input('Do you want to download the model? y/[n] : ')
+        if reply.lower() == 'y':
+            fname = prepare_ft_model()
+        elif reply == '' or reply.lower() == 'n':
+            print('Quitting.')
+            sys.exit()
+        else:
+            raise ValueError(f"Unrecognized option {reply}.")
+        
+    # Load fasttext model once for all datasets.
     print('Loading fasttext model...')
     model = fasttext.load_model(fname)
     # model=None
